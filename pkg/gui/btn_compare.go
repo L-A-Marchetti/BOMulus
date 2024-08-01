d709aebaaad6f4d39031299ca94077e76402ffff
lmarchet <lucasmarchetti21@gmail.com>
2024-08-01 10:29:02 +0200

    scrollbox reset fixed

--- pkg/gui/btn_compare.go
+++ pkg/gui/btn_compare.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+var scrolledVBox *gtk.Box // Déclaration globale pour conserver la référence de la nouvelle vBox
+
 func BtnCompare(button *gtk.Button) {
 	// Check if there are two files.
 	if core.XlsmFiles[0].Path == config.INIT_FILE_PATH_1 || core.XlsmFiles[1].Path == config.INIT_FILE_PATH_2 {
@@ -34,16 +36,23 @@ func BtnCompare(button *gtk.Button) {
 	scrolledWindow.SetHExpand(true)
 	// Enlarge scrollbars.
 	EnlargeSb()
-	// Remove any existing TreeView from vBox and add the new one
-	children := vBox.GetChildren()
-	for l := children; l != nil; l = l.Next() {
-		child := l.Data()
-		if sw, ok := child.(*gtk.ScrolledWindow); ok {
-			vBox.Remove(sw)
-		}
+
+	// Remove the existing scrolledVBox if it exists
+	if scrolledVBox != nil {
+		vBox.Remove(scrolledVBox)
 	}
-	vBox.PackStart(diffSummaryLabel, false, false, 0)
-	vBox.PackStart(scrolledWindow, true, true, 0)
+
+	// Create a new vBox for the ScrolledWindow
+	scrolledVBox, err = gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		panic(err)
+	}
+	scrolledVBox.PackStart(diffSummaryLabel, false, false, 0)
+	scrolledVBox.PackStart(scrolledWindow, true, true, 0)
+
+	// Add the new elements
+	//vBox.PackStart(diffSummaryLabel, false, false, 0)
+	vBox.PackStart(scrolledVBox, true, true, 0)
 	vBox.ShowAll()
 	Output()
 }
