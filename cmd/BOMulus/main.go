fd8678c0ff4046c8e72e27f7e1805254923c24fe
lmarchet <lucasmarchetti21@gmail.com>
2024-08-04 17:24:45 +0200

    Try to create env before the execution

--- cmd/BOMulus/main.go
+++ cmd/BOMulus/main.go
@@ -2,7 +2,29 @@ package main
 
 import (
 	"gui"
+	"os"
+	"path/filepath"
+	"runtime"
 )
+
+func init() {
+	if runtime.GOOS == "windows" {
+		// Obtenez le chemin de l'exécutable
+		exePath, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+
+		// Construisez le chemin vers le dossier des schémas
+		schemaDir := filepath.Join(filepath.Dir(exePath), "share", "glib-2.0", "schemas")
+
+		// Définissez la variable d'environnement
+		err = os.Setenv("GSETTINGS_SCHEMA_DIR", schemaDir)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 func main() {
 	gui.GuiInit()
