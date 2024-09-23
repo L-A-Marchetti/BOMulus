83b5820d9d5de5cbf638a2208865a19a65dcae05
Octavelscx <octavelascoux@gmail.com>
2024-09-23 14:35:33 +0200

    Clean the content of the vbox by removing its children rather than deleting and recreating the vbox itself.

--- pkg/gui/compare_view.go
+++ pkg/gui/compare_view.go
@@ -16,24 +16,37 @@ var (
 )
 
 func compareView() {
-	if config.DEBUGGING {
-		defer core.StartBenchmark("gui.compareView()", true).Stop()
-	}
-	// Create a new window for showing the report.
-	oldName, newName := strings.Split(core.XlsmFiles[0].Path, "/"), strings.Split(core.XlsmFiles[1].Path, "/")
-	compareTitle := fmt.Sprintf("%s/%s", strings.Split(oldName[len(oldName)-1], ".")[0], strings.Split(newName[len(newName)-1], ".")[0])
-	if compareWindow != nil {
-		scrolledWindow.Remove(vbox)
-		compareWindow.SetTitle(compareTitle)
-	} else {
-		compareWindow = createWindow(compareTitle, 1200, 900)
-		scrolledWindow = createCommonScrolledWindow()
-		compareWindow.Add(scrolledWindow)
-	}
-	vbox = createBox(gtk.ORIENTATION_VERTICAL, 10)
-	addBoxMargin(vbox)
-	scrolledWindow.Add(vbox)
-	compareHeader(vbox)
-	createCompareGrid(vbox)
-	compareWindow.ShowAll()
+    if config.DEBUGGING {
+        defer core.StartBenchmark("gui.compareView()", true).Stop()
+    }
+
+    // Create a new window for showing the comparison.
+    oldName, newName := strings.Split(core.XlsmFiles[0].Path, "/"), strings.Split(core.XlsmFiles[1].Path, "/")
+    compareTitle := fmt.Sprintf("%s/%s", strings.Split(oldName[len(oldName)-1], ".")[0], strings.Split(newName[len(newName)-1], ".")[0])
+
+    if compareWindow == nil {
+        compareWindow = createWindow(compareTitle, 1200, 900)
+        compareWindow.Connect("destroy", func() {
+            compareWindow = nil
+            scrolledWindow = nil
+            vbox = nil
+        })
+        scrolledWindow = createCommonScrolledWindow()
+        compareWindow.Add(scrolledWindow)
+        vbox = createBox(gtk.ORIENTATION_VERTICAL, 10)
+        addBoxMargin(vbox)
+        scrolledWindow.Add(vbox)
+    } else {
+        compareWindow.SetTitle(compareTitle)
+        // Empty the vbox instead of removing and adding it again. 
+        children := vbox.GetChildren()
+        children.Foreach(func(item interface{}) {
+            widget := item.(*gtk.Widget)
+            vbox.Remove(widget)
+        })
+    }
+
+    compareHeader(vbox)
+    createCompareGrid(vbox)
+    compareWindow.ShowAll()
 }
