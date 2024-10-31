6e2893fcf66a942a59ac1f6db284cc163762fc99
lmarchet <lucasmarchetti21@gmail.com>
2024-10-31 18:59:39 +0100

    hot fix api

--- config/API_key.go
+++
@@ -1,7 +0,0 @@
-package config
-
-const API_KEY string = "80bce682-d50c-4dca-b43e-c316b04e6060"
-
-// a5ed8d21-eb53-4ab3-85fa-35284bb9689d
-// c2c23773-01fc-43ea-9a17-e93be9493912
-//  80bce682-d50c-4dca-b43e-c316b04e6060
