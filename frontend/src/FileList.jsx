dd5614f2966851576367462e27a2e70490494e8e
lmarchet <lucasmarchetti21@gmail.com>
2024-11-05 09:41:35 +0100

    ui delete bom btn

--- frontend/src/FileList.jsx
+++ frontend/src/FileList.jsx
@@ -22,15 +22,22 @@ const FileList = ({ files, selectedFiles, onSelectFile }) => {
     return (
         <>
             {files.map((file) => (
-                <Button 
-                    key={file.path} 
-                    onClick={() => onSelectFile(file)} 
-                    className={`file-button ${selectedFiles.includes(file) ? 'selected' : ''}`}
-                >
-                    {selectedFiles.includes(file) 
-                        ? `${selectedFiles.length === 1 ? "single" : selectedFiles.indexOf(file) === 0 ? "v1" : "v2"} ${getFileName(file.path)}`
-                        : getFileName(file.path)}
-                </Button>
+                <div className='file-btns'>
+                    <Button 
+                        key={file.path} 
+                        onClick={() => onSelectFile(file)} 
+                        className={`file-button ${selectedFiles.includes(file) ? 'selected' : ''}`}
+                    >
+                        {selectedFiles.includes(file) 
+                            ? `${selectedFiles.length === 1 ? "single" : selectedFiles.indexOf(file) === 0 ? "v1" : "v2"} ${getFileName(file.path)}`
+                            : getFileName(file.path)}
+                    </Button>
+                    <Button
+                        className={`delete-button`}    
+                    >
+                        ⌫
+                    </Button>
+                </div>
             ))}
         </>
     );
