dc4566294fd6688965d94272c5713db6d5d8dec8
lmarchet <lucasmarchetti21@gmail.com>
2025-02-08 15:26:04 +0100

    new type

--- frontend/src/store/GlobalStore.tsx
+++ frontend/src/store/GlobalStore.tsx
@@ -1,11 +1,23 @@
 import { create } from 'zustand';
 
+type workspace = {
+  id: number;
+  name: string;
+}
+
 interface WSChooserProps {
+  workspaces: workspace[];
   isVisible: boolean;
+  isLoading: boolean;
+  error: string;
   toggleVisibility: () => void;
+  //loadWorkspaces: () => void;
 }
 
 export const WSChooserStore = create<WSChooserProps>((set) => ({
+  workspaces: [],
   isVisible: true,
+  isLoading: true,
+  error: "",
   toggleVisibility: () => set((state) => ({ isVisible: !state.isVisible })),
 }));
