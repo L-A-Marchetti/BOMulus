9aa3ca68270ac7c75916d4d118e1cc259f6e478f
lmarchet <lucasmarchetti21@gmail.com>
2025-02-08 15:45:52 +0100

    replace my types

--- frontend/src/store/GlobalStore.tsx
+++ frontend/src/store/GlobalStore.tsx
@@ -1,9 +1,31 @@
 import { create } from 'zustand';
+import { GetRecentWorkspaces } from "../../wailsjs/go/main/App";
 
-type workspace = {
-  id: number;
+type workspaceInfos = {
   name: string;
-}
+  path: string;
+  createdAt: string;
+  last_opened: string;
+  production_quantity: string;
+  last_comparison: {
+    v1: string;
+    v2: string;
+  };
+};
+
+type fileInfo = {
+  version_tag: number;
+  name: string;
+  path: string;
+  components: any[];
+  filters: any; 
+};
+
+export type workspace = {
+  workspace_infos: workspaceInfos,
+  files: fileInfo[],
+};
+
 
 interface WSChooserProps {
   workspaces: workspace[];
@@ -11,7 +33,7 @@ interface WSChooserProps {
   isLoading: boolean;
   error: string;
   toggleVisibility: () => void;
-  //loadWorkspaces: () => void;
+  loadWorkspaces: () => void;
 }
 
 export const WSChooserStore = create<WSChooserProps>((set) => ({
@@ -20,4 +42,13 @@ export const WSChooserStore = create<WSChooserProps>((set) => ({
   isLoading: true,
   error: "",
   toggleVisibility: () => set((state) => ({ isVisible: !state.isVisible })),
+  loadWorkspaces: async () => {
+    set({ isLoading: true, error: "" });
+    try {
+      const workspaces: workspace[] = await GetRecentWorkspaces(); // Directement typé
+      set({ workspaces, isLoading: false });
+    } catch (err) {
+      set({ error: "Failed to load workspaces", isLoading: false });
+    }
+  },
 }));
